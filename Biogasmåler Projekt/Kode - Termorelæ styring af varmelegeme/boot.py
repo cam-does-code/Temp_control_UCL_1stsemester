ac3ad202940b64d7d06689deab80499e6a0670a9
BennyN86 <benzdk@gmail.com>
2023-01-30 14:38:32 +0100

    Co-authored-by: Cam <cami834j@gmail.com>

--- Biogasmåler Projekt/Kode - Termorelæ styring af varmelegeme/boot.py
+++ Biogasmåler Projekt/Kode - Termorelæ styring af varmelegeme/boot.py
@@ -6,6 +6,8 @@ import ssd1306
 
 # ESP32 GPIO 32
 relay = Pin(32, Pin.OUT)
+
+
 # relay_pump = Pin(26, Pin.OUT)
 # sensor = dht.DHT11(Pin(2))
 ds_pin = machine.Pin(2)
@@ -14,7 +16,7 @@ ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
 temp_up = Pin(26, Pin.IN)
 temp_down = Pin(25, Pin.IN)
 enter = Pin(27, Pin.IN)
-on_off = Pin(28, Pin.IN)
+# on_off = Pin(28, Pin.IN)
 
 dat = machine.Pin(22)       # the device is on GPIO0
 i2c = I2C(scl=Pin(22), sda=Pin(21), freq=100000)      #Init i2c
@@ -22,6 +24,8 @@ lcd=ssd1306.SSD1306_I2C(128,64,i2c)           #create LCD object,Specify col and
 
 roms = ds_sensor.scan()
 print('Found DS devices: ', roms)
+
+set_temp = 20
 
 # while True:
 #     temp = ds_sensor.read_temp
@@ -31,10 +35,26 @@ print('Found DS devices: ', roms)
 #     elif ds_sensor.read_temp < 24:
 #         relay.value(1)
 #     sleep(2)
-set_temp = 20
 print("set temp")
-def set_temperature():
-    while True:
+while True:
+    lcd.fill(0)
+    lcd.text("Choose temp:",10,16) 
+    lcd.text(str(set_temp),10,40)
+    lcd.show()
+    if temp_up.value() == 1:
+        set_temp += 1
+        print(set_temp)
+    elif temp_down.value() == 1:
+        set_temp -= 1
+        print(set_temp)
+    elif enter.value() == 1:
+        break
+    else:
+        continue
+
+while True:
+    if enter.value() == 1:
+        while True:
             lcd.fill(0)
             lcd.text("Choose temp:",10,16) 
             lcd.text(str(set_temp),10,40)
@@ -49,37 +69,32 @@ def set_temperature():
                 break
             else:
                 continue
-
-def run():
-    while True:
-        if enter.value() == 1:
-            set_temperature()
-        if on_off.value() == 1:
-            break
+        # if on_off.value() == 1:
+        #     break
         # if temp_up.value() == 1:
         #     set_temp += 1
         # if temp_down.value() == 1:
         #     set_temp -= 1
         # if set_temp == -100:
         #     set_temp = float(input("Please enter desired temperature: "))
-        ds_sensor.convert_temp()
+    ds_sensor.convert_temp()
         #time.sleep(1)
-        for rom in roms:
-            print(rom)
-            print(ds_sensor.read_temp(rom))
-            #time.sleep(1) 
-            lcd.fill(0)
-            lcd.text("Set temp:" + str(set_temp),10,16) 
-            lcd.text("Current:" + str(ds_sensor.read_temp(rom)),10,40)
-            lcd.show() 
-            if ds_sensor.read_temp(rom) >= set_temp:
-                relay.value(0)
-            elif ds_sensor.read_temp(rom) < set_temp:
-                relay.value(1)
-        time.sleep(1)
+    for rom in roms:
+        print(rom)
+        print(ds_sensor.read_temp(rom))
+        #time.sleep(1) 
+        lcd.fill(0)
+        lcd.text("Set temp:" + str(set_temp),10,16) 
+        lcd.text("Current:" + str(ds_sensor.read_temp(rom)),10,40)
+        lcd.show() 
+        if ds_sensor.read_temp(rom) >= set_temp:
+            relay.value(0)
+        elif ds_sensor.read_temp(rom) < set_temp:
+            relay.value(1)
+    time.sleep(1)
 
-set_temperature()
-run()
+#set_temp = set_temperature(set_temp)
+#run()
 
 # while True:
 #   # RELAY ON
